f2c255268d380906b25e5d6297692b8b845e663f
Dear <natthaphong.ea@gmail.com>
2024-07-24 14:58:17 +0700

    feat: complete ex.1 and ex.2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,36 @@
 function sortCustomerName(customers) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+
+  // let left = '';
+  // let right = '';
+  let temp = '';
+  for (let i = 0; i < customers.length; i++) {
+    if (customers[i] > customers[i + 1]) {
+      temp = customers[i];
+      customers[i] = customers[i + 1];
+      customers[i + 1] = temp;
+    }
+  }
+  for (let i = 0; i < customers.length; i++) {
+    if (customers[i] > customers[i + 1]) {
+      temp = customers[i];
+      customers[i] = customers[i + 1];
+      customers[i + 1] = temp;
+    }
+  }
+
+  return customers;
 }
 
+let customers = ['Somchai', 'Arthit', 'Suchada', 'Napasorn', 'Pimchanok'];
+
+console.log(sortCustomerName(customers));
+//// [ "Arthit", "Napasorn", "Pimchanok", "Somchai", "Suchada"]
+//[ 'Arthit', 'Somchai', 'Napasorn', 'Pimchanok', 'Suchada' ]
+customers = ['Arthit', 'Somchai', 'Napasorn', 'Pimchanok', 'Suchada'];
+
+console.log(sortCustomerName(customers));
+// ['Arthit', 'Napasorn', 'Pimchanok', 'Somchai', 'Suchada'];
+
 // ตอบคำถามตรงนี้จ้า
+// จำสูตร bubble sort ไม่ได้ แต่ที่เขียนไปเป็นการใช้ Loop 2 รอบ เป็น Big O n กำลัง 2 ครับ
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,50 @@
 function sortStudentsByScore(students) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+
+  let temp = {};
+  for (let i = 0; i < students.length; i++) {
+    if (students[i]?.score < students[i + 1]?.score) {
+      temp = students[i];
+      students[i] = students[i + 1];
+      students[i + 1] = temp;
+    }
+  }
+  for (let i = 0; i < students.length; i++) {
+    if (students[i]?.score < students[i + 1]?.score) {
+      temp = students[i];
+      students[i] = students[i + 1];
+      students[i + 1] = temp;
+    }
+  }
+  for (let i = 0; i < students.length; i++) {
+    if (students[i]?.score < students[i + 1]?.score) {
+      temp = students[i];
+      students[i] = students[i + 1];
+      students[i + 1] = temp;
+    }
+  }
+
+  return students;
 }
+
+let students = [
+  { id: 'S001', name: 'Anan', score: 85 },
+  { id: 'S002', name: 'Boon', score: 92 },
+  { id: 'S003', name: 'Chai', score: 78 },
+  { id: 'S004', name: 'Dao', score: 95 },
+  { id: 'S005', name: 'Ek', score: 88 },
+  { id: 'S006', name: 'Fah', score: 80 }
+];
+// console.log(students[1].score);
+
+console.log(sortStudentsByScore(students));
+// [
+//   { id: "S004", name: "Dao", score: 95 },
+//   { id: "S002", name: "Boon", score: 92 },
+//   { id: "S005", name: "Ek", score: 88 },
+//   { id: "S001", name: "Anan", score: 85 },
+//   { id: "S006", name: "Fah", score: 80 },
+//   { id: "S003", name: "Chai", score: 78 }
+// ]
+
+///// จำสูตร bubbles ไม่ได้จริงๆคร้าบ เดวกลับไปทวนนะครับ T T
